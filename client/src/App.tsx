261d5bc6284caad35c7d8e1281a8408f8801f67d
Kashif Shabih <kashif.shabih@hotmail.com>
2025-04-17 19:32:32 -0500

    Fetch data from API

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,24 @@
+import { useEffect, useState } from 'react';
+
 function App() {
-  return <h3>Reactivities</h3>;
+  const [activities, setActivities] = useState<Activity[]>([]);
+
+  useEffect(() => {
+    fetch('https://localhost:5001/api/activities')
+      .then((response) => response.json())
+      .then((data) => setActivities(data));
+  }, []);
+
+  return (
+    <div>
+      <h3>Reactivities</h3>
+      <ul>
+        {activities.map((activity) => (
+          <li key={activity.id}>{activity.title}</li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default App;
