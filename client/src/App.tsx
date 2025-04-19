947a4128d78e02cf135cfdf7ffcee89cdf9e663c
Kashif Shabih <kashif.shabih@hotmail.com>
2025-04-19 18:15:42 -0500

    Add material UI

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,3 +1,4 @@
+import { List, ListItem, ListItemText, Typography } from '@mui/material';
 import { useEffect, useState } from 'react';
 
 function App() {
@@ -10,14 +11,16 @@ function App() {
   }, []);
 
   return (
-    <div>
-      <h3>Reactivities</h3>
-      <ul>
+    <>
+      <Typography variant="h3">Reactivities</Typography>
+      <List>
         {activities.map((activity) => (
-          <li key={activity.id}>{activity.title}</li>
+          <ListItem key={activity.id}>
+            <ListItemText>{activity.title}</ListItemText>
+          </ListItem>
         ))}
-      </ul>
-    </div>
+      </List>
+    </>
   );
 }
 
