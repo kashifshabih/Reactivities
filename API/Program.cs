e10ba8d7d63ece1a6ca45690f3ef415e6c1024a0
Kashif Shabih <kashif.shabih@hotmail.com>
2025-04-20 15:49:59 -0500

    Add automapper

--- API/Program.cs
+++ API/Program.cs
@@ -1,4 +1,5 @@
 using Application.Activities.Queries;
+using Application.Core;
 using Microsoft.EntityFrameworkCore;
 using Persistence;
 
@@ -11,8 +12,9 @@ builder.Services.AddDbContext<AppDbContext>(opt =>
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 builder.Services.AddCors();
-builder.Services.AddMediatR(x => x.RegisterServicesFromAssemblyContaining<GetActivityList.Handler>());
-
+builder.Services.AddMediatR(x => 
+    x.RegisterServicesFromAssemblyContaining<GetActivityList.Handler>());
+builder.Services.AddAutoMapper(typeof(MappingProfiles).Assembly);
 
 var app = builder.Build();
 
--- Application/Activities/Commands/EditActivity.cs
+++ Application/Activities/Commands/EditActivity.cs
@@ -1,3 +1,4 @@
+using AutoMapper;
 using Domain;
 using MediatR;
 using Persistence;
@@ -11,7 +12,7 @@ public class EditActivity
         public required Activity Activity { get; set; }
     }
 
-    public class Handler(AppDbContext context) : IRequestHandler<Command>
+    public class Handler(AppDbContext context, IMapper mapper) : IRequestHandler<Command>
     {
         public async Task Handle(Command request, CancellationToken cancellationToken)
         {
@@ -19,7 +20,7 @@ public class EditActivity
                 .FindAsync([request.Activity.Id], cancellationToken) 
                     ?? throw new Exception("Cannot find activity");
 
-           activity.Title = request.Activity.Title;
+            mapper.Map(request.Activity, activity);
 
             await context.SaveChangesAsync(cancellationToken);
         }
