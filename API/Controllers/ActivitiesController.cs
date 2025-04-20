fd37795e28c0ceddf57aabe48afa1df85f74dede
Kashif Shabih <kashif.shabih@hotmail.com>
2025-04-20 18:38:44 -0500

    Add mediator handler to delete activity

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -31,5 +31,13 @@ public class ActivitiesController() : BaseApiController
         await Mediator.Send(new EditActivity.Command{Activity = activity});
 
         return NoContent();
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<ActionResult> DeleteActivity(string id)
+    {
+        await Mediator.Send(new DeleteActivity.Command{Id = id});
+
+        return Ok();
     }    
 }
