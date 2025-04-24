497b85b380b4ac2851c447283e299e1c7c945985
Kashif Shabih <kashif.shabih@hotmail.com>
2025-04-23 21:47:23 -0500

    Add / Update an activity

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -35,6 +35,19 @@ function App() {
     setEditMode(false);
   };
 
+  const handleSubmitForm = (activity: Activity) => {
+    if (activity.id) {
+      setActivities(
+        activities.map((x) => (x.id === activity.id ? activity : x))
+      );
+    } else {
+      const newActivity = { ...activity, id: activities.length.toString() };
+      setSelectedActivity(newActivity);
+      setActivities([...activities, newActivity]);
+    }
+    setEditMode(false);
+  };
+
   return (
     <Box sx={{ bgcolor: '#eeeeee' }}>
       <CssBaseline />
@@ -48,6 +61,7 @@ function App() {
           editMode={editMode}
           openForm={handleOpenForm}
           closeForm={handleFormClose}
+          submitForm={handleSubmitForm}
         />
       </Container>
     </Box>
