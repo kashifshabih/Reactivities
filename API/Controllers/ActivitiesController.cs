42c019c539f98bf204f13c47c96a8e4f4972eee4
Kashif Shabih <kashif.shabih@hotmail.com>
2025-05-02 14:04:13 -0500

    Implement error handling

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -1,11 +1,14 @@
+using System;
 using Application.Activities.Commands;
+using Application.Activities.DTOs;
 using Application.Activities.Queries;
 using Domain;
+using MediatR;
 using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers;
 
-public class ActivitiesController() : BaseApiController
+public class ActivitiesController : BaseApiController
 {
     [HttpGet]
     public async Task<ActionResult<List<Activity>>> GetActivities()
@@ -13,31 +16,27 @@ public class ActivitiesController() : BaseApiController
         return await Mediator.Send(new GetActivityList.Query());
     }
 
-  [HttpGet("{id}")]
+    [HttpGet("{id}")]
     public async Task<ActionResult<Activity>> GetActivityDetail(string id)
     {
-        return await Mediator.Send(new GetActivityDetails.Query{Id = id});
+        return HandleResult(await Mediator.Send(new GetActivityDetails.Query{Id = id}));
     }
 
     [HttpPost]
-    public async Task<ActionResult<string>> CreateActivity(Activity activity)
+    public async Task<ActionResult<string>> CreateActivity(CreateActivityDto activityDto)
     {
-        return await Mediator.Send(new CreateActivity.Command{Activity = activity});
+        return HandleResult(await Mediator.Send(new CreateActivity.Command{ActivityDto = activityDto}));
     }
 
     [HttpPut]
     public async Task<ActionResult> EditActivity(Activity activity)
     {
-        await Mediator.Send(new EditActivity.Command{Activity = activity});
-
-        return NoContent();
+        return HandleResult(await Mediator.Send(new EditActivity.Command{Activity = activity}));
     }
 
     [HttpDelete("{id}")]
     public async Task<ActionResult> DeleteActivity(string id)
     {
-        await Mediator.Send(new DeleteActivity.Command{Id = id});
-
-        return Ok();
-    }    
+        return HandleResult(await Mediator.Send(new DeleteActivity.Command{Id = id}));
+    }
 }
--- Application/Activities/Queries/GetActivityDetails.cs
+++ Application/Activities/Queries/GetActivityDetails.cs
@@ -1,27 +1,27 @@
 using System;
+using Application.Core;
 using Domain;
 using MediatR;
-using Microsoft.EntityFrameworkCore;
 using Persistence;
 
 namespace Application.Activities.Queries;
 
 public class GetActivityDetails
 {
-    public class Query : IRequest<Activity>
+    public class Query : IRequest<Result<Activity>>
     {
         public required string Id { get; set; }
     }
 
-    public class Handler(AppDbContext context) : IRequestHandler<Query, Activity>
+    public class Handler(AppDbContext context) : IRequestHandler<Query, Result<Activity>>
     {
-        public async Task<Activity> Handle(Query request, CancellationToken cancellationToken)
+        public async Task<Result<Activity>> Handle(Query request, CancellationToken cancellationToken)
         {
             var activity = await context.Activities.FindAsync([request.Id], cancellationToken);
 
-            if (activity == null) throw new Exception("Activity not found");
+            if (activity == null) return Result<Activity>.Failure("Activity not found", 404);
 
-            return activity;
+            return Result<Activity>.Success(activity);
         }
     }
 }
